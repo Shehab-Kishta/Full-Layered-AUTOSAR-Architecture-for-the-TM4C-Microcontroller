c72c386c9af93869cc24f14c640e8e47828ff2fa
Shehab Kishta <sh.kishta@gmail.com>
2024-11-14 10:12:24 +0200

    Update Button.c

--- AUTOSAR_Project_Shehab/Button.c
+++ AUTOSAR_Project_Shehab/Button.c
@@ -6,7 +6,7 @@
  *
  * Description: Source file for Button Module.
  *
- * Author: Mohamed Tarek
+ * Author: Shehab Kishta
  ******************************************************************************/
 #include "Dio.h"
 #include "Port.h"
