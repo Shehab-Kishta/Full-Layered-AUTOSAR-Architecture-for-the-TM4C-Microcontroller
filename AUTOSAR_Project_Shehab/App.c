c7f9d0d76a50251adce52edfbe04acee61d6c527
Shehab Kishta <sh.kishta@gmail.com>
2024-11-14 10:11:42 +0200

    Update App.c

--- AUTOSAR_Project_Shehab/App.c
+++ AUTOSAR_Project_Shehab/App.c
@@ -6,7 +6,7 @@
  *
  * Description: Source file for Application Tasks.
  *
- * Author: Mohamed Tarek
+ * Author: Shehab Kishta
  ******************************************************************************/
 
 #include "App.h"
