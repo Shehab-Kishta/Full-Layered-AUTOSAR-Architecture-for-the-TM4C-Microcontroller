54331ece7e27e667f64ba7d5a64cb6bf463c184a
Shehab Kishta <sh.kishta@gmail.com>
2024-11-14 10:16:04 +0200

    Update Gpt.h

--- AUTOSAR_Project_Shehab/Gpt.h
+++ AUTOSAR_Project_Shehab/Gpt.h
@@ -6,7 +6,8 @@
  *
  * Description: Header file for TM4C123GH6PM Microcontroller - SysTick Timer Driver.
  *
- * Author: Mohamed Tarek
+ * Author: Shehab Kishta
+ *
  ******************************************************************************/
 
 #ifndef GPT_H
