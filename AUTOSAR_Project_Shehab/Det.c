1c80a6220b8bb0b75978195eb3fbcb9fb296c737
Shehab Kishta <sh.kishta@gmail.com>
2024-11-14 10:14:05 +0200

    Update Det.c

--- AUTOSAR_Project_Shehab/Det.c
+++ AUTOSAR_Project_Shehab/Det.c
@@ -6,7 +6,8 @@
  *
  * Description:  Det stores the development errors reported by other modules.
  *
- * Author: Mohamed Tarek
+ * Author: Shehab Kishta
+ *
  ******************************************************************************/
 
 #include "Det.h"
